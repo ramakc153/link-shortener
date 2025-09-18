02fbb792178e48a86e84d7980765c7e2e97dc0b2
Rama Wandya <ramawandia153@gmail.com>
2025-09-18 12:44:39 +0700

    feat (main): add new route for testing FE

--- main.go
+++ main.go
@@ -6,6 +6,9 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "hello")
+	})
 	http.HandleFunc("/", Redirect)
 	http.HandleFunc("/shorted", Add_data)
 	addr := ":3001"
